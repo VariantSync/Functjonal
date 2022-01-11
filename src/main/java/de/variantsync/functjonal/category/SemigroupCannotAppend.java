0dd531e5f036e7f354cda874d421023b4408d7c8
pmbittner <paul.bittner@uni-ulm.de>
2022-01-11 17:21:45 +0100

    semigroups

---
+++ src/main/java/de/variantsync/functjonal/category/SemigroupCannotAppend.java
@@ -0,0 +1,7 @@
+package de.variantsync.functjonal.category;
+
+public class SemigroupCannotAppend extends RuntimeException {
+    public SemigroupCannotAppend(String message) {
+        super(message);
+    }
+}
