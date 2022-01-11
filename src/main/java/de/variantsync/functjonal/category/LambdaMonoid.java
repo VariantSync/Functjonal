b5ab182bb7276b7a5d7ad07f389d296baa305cac
pmbittner <paul.bittner@uni-ulm.de>
2022-01-11 17:22:18 +0100

    make monoid be a semigroup

--- src/main/java/de/variantsync/functjonal/category/LambdaMonoid.java
+++ src/main/java/de/variantsync/functjonal/category/LambdaMonoid.java
@@ -1,16 +1,15 @@
 package de.variantsync.functjonal.category;
 
-import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
-record LambdaMonoid<M>(Supplier<M> empty, BiFunction<M, M, M> compose) implements Monoid<M> {
+record LambdaMonoid<M>(Supplier<M> empty, Semigroup<M> compose) implements Monoid<M> {
     @Override
-    public M mEmpty() {
+    public M neutral() {
         return empty.get();
     }
 
     @Override
-    public M mAppend(M a, M b) {
-        return compose.apply(a, b);
+    public M append(M a, M b) {
+        return compose.append(a, b);
     }
 }
--- src/main/java/de/variantsync/functjonal/category/Monoid.java
+++ src/main/java/de/variantsync/functjonal/category/Monoid.java
@@ -1,13 +1,15 @@
 package de.variantsync.functjonal.category;
 
-import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
-public interface Monoid<M> {
-    M mEmpty();
-    M mAppend(final M a, final M b);
+/**
+ * A semigroup with a neutral element w.r.t. composition.
+ * @param <M> The type that forms a monoid.
+ */
+public interface Monoid<M> extends Semigroup<M> {
+    M neutral();
 
-    static <N> Monoid<N> From(final Supplier<N> empty, final BiFunction<N, N, N> compose) {
+    static <N> Monoid<N> From(final Supplier<N> empty, final Semigroup<N> compose) {
         return new LambdaMonoid<>(empty, compose);
     }
 }
