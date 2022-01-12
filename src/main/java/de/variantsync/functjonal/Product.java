8245509999b8849f1f730401271c770565a89a02
pmbittner <paul.bittner@uni-ulm.de>
2022-01-12 13:19:42 +0100

    monoid for products

--- src/main/java/de/variantsync/functjonal/Product.java
+++ src/main/java/de/variantsync/functjonal/Product.java
@@ -1,8 +1,24 @@
 package de.variantsync.functjonal;
 
+import de.variantsync.functjonal.category.Monoid;
+
 import java.util.function.Function;
 
 public record Product<A, B>(A first, B second) {
+    /**
+     * Products are monoids over monoidal values.
+     * Creates a Monoid for Product<A, B> from monoids of for A and B.
+     * @param ma Monoid over values of type A.
+     * @param mb Monoid over values of type B.
+     * @return a Monoid for Pair<A, B>.
+     */
+    public static <A, B> Monoid<Product<A, B>> MONOID(final Monoid<A> ma, final Monoid<B> mb) {
+        return Monoid.From(
+                () -> new Product<>(ma.neutral(), mb.neutral()),
+                (a, b) -> new Product<>(ma.append(a.first, b.first), mb.append(a.second, b.second))
+        );
+    }
+
     public <A2> Product<A2, B> mapFirst(final Function<? super A, ? extends A2> f) {
         return new Product<>(f.apply(first), second);
     }
