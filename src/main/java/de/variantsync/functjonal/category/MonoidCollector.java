6d9a91fbb2bf4ff3470f872ddba2f31a4df298af
pmbittner <paul.bittner@uni-ulm.de>
2022-02-22 15:14:45 +0100

    bugfixes

--- src/main/java/de/variantsync/functjonal/category/MonoidCollector.java
+++ src/main/java/de/variantsync/functjonal/category/MonoidCollector.java
@@ -1,15 +1,16 @@
 package de.variantsync.functjonal.category;
 
 class MonoidCollector<M> {
-    private Monoid<M> monoid;
+    private final Monoid<M> monoid;
     private M val;
 
     public MonoidCollector(final Monoid<M> m) {
         this.val = m.neutral();
+        this.monoid = m;
     }
 
     void accumulate(final M other) {
-        val = monoid.append(val, val);
+        val = monoid.append(val, other);
     }
 
     MonoidCollector<M> combine(final MonoidCollector<M> other) {
